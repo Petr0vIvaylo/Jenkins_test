3c1a355d0314bf1a41c1fb66f54cedcb4afd5f6e
Ivo Petrov <60368385+Petr0vIvaylo@users.noreply.github.com>
2022-02-15 13:04:07 +0200

    Update PrimeService_IsPrimeShould.cs

--- PrimeService_IsPrimeShould.cs
+++ PrimeService_IsPrimeShould.cs
@@ -7,18 +7,30 @@ namespace Prime.UnitTests.Services
     public class PrimeService_IsPrimeShould
     {
         private readonly PrimeService _primeService;
-
+        
+        public bool IsPrime(int candidate)
+        {
+            if (candidate == 1)
+            {
+                return false;
+            }
+            return true;
+        }
+        
         public PrimeService_IsPrimeShould()
         {
             _primeService = new PrimeService();
         }
 
         [TestMethod]
-        public void IsPrime_InputIs1_ReturnFalse()
+        [DataRow(-1)]
+        [DataRow(0)]
+        [DataRow(1)]
+        public void IsPrime_InputIs1_ReturnFalse(int value)
         {
-            bool result = _primeService.IsPrime(1);
+            bool result = _primeService.IsPrime(value);
 
-            Assert.IsFalse(result, "1 should not be prime");
+            Assert.IsFalse(result, $"{value} should not be prime");
         }
     }
 }
